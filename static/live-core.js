6009ff60e49d94fa736ff6bca70c2bf41b722719
Mateusz Kmiecik <mk.chomik@gmail.com>
2011-12-17 15:50:31 +0100

    Added support for frame resizing (#2525)

--- static/live-core.js
+++ static/live-core.js
@@ -212,6 +212,10 @@ SymPy.Shell = Ext.extend(Ext.util.Observable, {
             this.promptEl.focus();
         }, this);
 
+        this.fullscreenEl.on("click", function(event) {
+            this.fullscreen();
+        }, this);
+
         this.clearEl.on("click", function(event) {
             this.clear();
             this.promptEl.focus();
@@ -326,14 +330,18 @@ SymPy.Shell = Ext.extend(Ext.util.Observable, {
                     tag: 'option',
                     value: 'off',
                     html: 'Off'
+                }]
+            }, {
+                tag: 'button',
+                html: 'Fullscreen'
             }]
-        }]
         }, true);
 
         this.supportsSelection = ('selectionStart' in this.promptEl.dom);
 
         this.evaluateEl = this.toolbarEl.down('button:nth(1)');
         this.clearEl = this.toolbarEl.down('button:nth(2)');
+        this.fullscreenEl = this.toolbarEl.down('button:nth(3)');
 
         this.printerEl = this.toolbarEl.down('select:nth(1)');
         this.submitEl = this.toolbarEl.down('select:nth(2)');
@@ -772,5 +780,107 @@ SymPy.Shell = Ext.extend(Ext.util.Observable, {
         }
 
         return (result) ? result : default_value;
+    },
+
+    fullscreen: function() {
+
+        var popup = $('<div id="fs-popup">Escape to close fullscreen mode.</div>');
+        popup.css({
+            'font-size': 20,
+            'color' : '#fff',
+            'z-index' : 1000,
+            'position' : 'absolute'
+        });
+
+        var shell = $('#shell'),
+            leftdiv = $('#left'),
+            ld = {
+                pos : leftdiv.offset(),
+                width : '55%',
+                height : leftdiv.height(),
+                border : 2
+            };
+
+        function fsmake(){
+            //browser viewport dimensions
+            var bheight = $(window).height(),
+                bwidth = $(window).width();
+            
+            leftdiv.css({
+                'margin' : 0,
+                'position' : 'absolute',
+                'z-index' : 500
+            }).animate({
+                'width' : bwidth,
+                'height' : bheight,
+                'top' : 0,
+                'left' : 0,
+                'border-width' : 0,
+                'padding' : 0
+            }, 100);
+            $('.sympy-live-output').css({
+                'width' : bwidth-32,
+                'height' : bheight-250
+            });
+        }
+
+        // some styles to make it look better
+        leftdiv.css({
+            'top' : ld.pos.top,
+            'left' : ld.pos.left
+        });
+        shell.css('padding', 10);
+        $('body').css('overflow', 'hidden');
+        $('.right_title').css('padding-top', 20);
+
+        $('html, body').animate({ scrollTop: 0 }, 100);
+        fsmake();
+
+        // window resizing -> new dimensions
+        
+        $(window).on("resize", function() {
+            clearTimeout(id);
+            id = setTimeout(function(){
+                fsmake();
+            }, 200);
+        });
+        
+        $(popup).appendTo('body').hide().fadeIn(500).delay(1000).fadeOut(500);
+
+        // enabling escape key to close fullscreen mode
+        var keyEvent = this.getKeyEvent();
+        Ext.get(document).on(keyEvent, function(event) {
+            if(event.getKey() == 27){
+                $(window).off("resize");
+                this.closefs(ld);
+            }
+        }, this);
+    },
+
+    closefs : function(ld) {
+        var shell = $('#shell'),
+            leftdiv = $('#left');
+        this.fs = false;
+        fs = false;
+        $('#shell').css('padding', 0);
+        $('body').css('overflow', 'auto');
+        $('.right_title').css('padding-top', 0);
+        
+        leftdiv.css({
+           position : 'static',
+           margin : '4px 0 4px 4px'
+        });
+        leftdiv.animate({
+            top : ld.pos.top,
+            left : ld.pos.left,
+            width : ld.width,
+            height : ld.height,
+            borderWidth : ld.border,
+            padding: 10
+        }, 100);
+        $('.sympy-live-output').css({
+            'width' : '95%',
+            'height' : '20em'
+        });
     }
 });
