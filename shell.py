677fe4280e754d5e53e48206890c47a819fca61f
Steve Anton <anxuiz.nx@gmail.com>
2012-01-12 21:04:20 -0800

    Use pickle version 1 instead of -1 (latest).

--- shell.py
+++ shell.py
@@ -466,7 +466,7 @@ class Session(db.Model):
     """
     # We need to disable the pickling optimization here in order to get the
     # correct values out.
-    blob = db.Blob(self.fast_dumps(value, -1))
+    blob = db.Blob(self.fast_dumps(value, 1))
 
     if name in self.global_names:
       index = self.global_names.index(name)
