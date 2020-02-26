2cf458bd1be7d575717a3b1527b4791fda46183e
prshnt19 <prashant.rawat216@gmail.com>
2020-02-27 01:24:06 +0530

    django admin staticfiles

--- sympy_live_django/settings.py
+++ sympy_live_django/settings.py
@@ -148,6 +148,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
