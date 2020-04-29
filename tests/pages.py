f208ca40f1cd26b2719f3988c94e1b7ad846a002
Amit Kumar <dtu.amit@gmail.com>
2020-04-29 16:24:36 +0100

    Add sidebar tests

--- tests/pages.py
+++ tests/pages.py
@@ -2,6 +2,7 @@
 This module contains SymPyLivePage,
 the page object for the SymPy Live's Home page
 """
+from selenium.webdriver.common.by import By
 
 
 class SymPyLivePage(object):
@@ -16,3 +17,10 @@ class SymPyLivePage(object):
 
     def title(self):
         return self.browser.title
+
+    def sidebar_headings(self):
+        sidebar_elements = self.browser.find_elements(By.CLASS_NAME, 'sidebar_card')
+        sidebar_headings = [
+            str(card.find_element(By.TAG_NAME, 'h3').text) for card in sidebar_elements
+        ]
+        return sidebar_headings
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,16 @@ def test_page_title(browser):
     live_page = SymPyLivePage(browser)
     live_page.load()
     assert live_page.title() == 'SymPy Live'
+
+
+def test_sidebar_loaded(browser):
+    live_page = SymPyLivePage(browser)
+    live_page.load()
+    side_bar_headings = live_page.sidebar_headings()
+    assert side_bar_headings == [
+        'Log In',
+        'About this page',
+        'Example session',
+        'Settings',
+        'Recent Statements'
+    ]
