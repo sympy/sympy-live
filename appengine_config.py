19a3b5498b29cae773a4007232d2e34ef4463712
Amit Kumar <dtu.amit@gmail.com>
2021-01-21 20:16:48 +0000

    Fix pkg_resouce import

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,4 @@
 import os
-import pkg_resources
 from google.appengine.ext import vendor
 
 # Set path to your libraries folder.
@@ -7,4 +6,6 @@ path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 # Add libraries installed in the path folder.
 vendor.add(path)
 # Add libraries to pkg_resources working set to find the distribution.
+
+import pkg_resources
 pkg_resources.working_set.add_entry(path)
